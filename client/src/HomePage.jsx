e72703eedc4e096060c1c22c3b8b64044abc7e16
NithishMR <145840454+NithishMR@users.noreply.github.com>
2024-11-02 20:22:57 +0530

    Added guest and route navigation

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -3,6 +3,7 @@ import SideBar from "./Components/SideBar";
 
 const HomePage = ({ user }) => {
   console.log(user);
+
   return (
     <div className="flex h-[100vh]">
       <div className="w-16 fixed top-0 left-0 h-full">
@@ -12,7 +13,7 @@ const HomePage = ({ user }) => {
       <div className="flex-1 ml-16 overflow-auto flex items-center justify-center">
         <div className="text-center">
           <h1 className="text-9xl bg-gradient-to-r from-violet-500 via-purple-500 to-blue-500 bg-clip-text text-transparent welcomeMessage animate-message">
-            Hello {user.usrname}
+            Hello {user ? user.usrname : "Guest"}
           </h1>
         </div>
       </div>
